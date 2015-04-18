3bbfe30408e9d1150d72201f826cf5837296a063
mochalygin <mochalygin@gmail.com>
2015-04-18 19:33:30 +0300

    controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,34 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/getTours", name="homepage")
+     */
+    public function getToursAction()
+    {
+        return new JsonResponse([1 => 'Ознакомительный тур по Вологде', 2 => 'Ознакомительный тур по Череповцу']);
+    }
+
+    /**
+     * @Route("/getTourDetails/{tourId}")
+     */
+    public function getTourDetailsAction($tourId)
+    {
+        return new JsonResponse([
+            ['name' => 'Точка 1', 'latitude' => 39.958789, 'longitude' => 59.212444],
+            ['name' => 'Точка 2', 'latitude' => 39.959789, 'longitude' => 59.222444],
+        ]);
     }
 }
